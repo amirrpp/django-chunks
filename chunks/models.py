6f1d90fae11eae8d349846a53eb081705a360984
amirrpp <pluton@freeday.com.ua>
2019-05-23 15:38:29 +0300

    py3

--- chunks/models.py
+++ chunks/models.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 from django.db import models
+from django.utils import six
 from django.utils.translation import ugettext_lazy as _
 
+
+@six.python_2_unicode_compatible
 class Chunk(models.Model):
     """
     A Chunk is a piece of content associated
@@ -9,13 +12,13 @@ class Chunk(models.Model):
     any template with the use of a special template
     tag
     """
-    key = models.CharField(_(u'Key'), help_text=_(u"A unique name for this chunk of content"), blank=False, max_length=255, unique=True)
-    content = models.TextField(_(u'Content'), blank=True)
-    description = models.CharField(_(u'Description'), blank=True, max_length=64, help_text=_(u"Short Description"))
+    key = models.CharField(_('Key'), help_text=_("A unique name for this chunk of content"), blank=False, max_length=255, unique=True)
+    content = models.TextField(_('Content'), blank=True)
+    description = models.CharField(_('Description'), blank=True, max_length=64, help_text=_("Short Description"))
 
     class Meta:
-        verbose_name = _(u'chunk')
-        verbose_name_plural = _(u'chunks')
+        verbose_name = _('chunk')
+        verbose_name_plural = _('chunks')
 
-    def __unicode__(self):
-        return u"%s" % (self.key,)
+    def __six__(self):
+        return "%s" % (self.key,)
